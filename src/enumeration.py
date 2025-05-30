541e2b3ff68d3425c19b5c8c74a66fad5c1181d5
Soraya Ezazipour <88604289+SorayaEzazipour@users.noreply.github.com>
2025-05-30 13:57:54 -0500

    Add files via upload

--- src/enumeration.py
+++ src/enumeration.py
@@ -23,33 +23,31 @@ def callback_function(m, where):
     U = m._U
     root = m._root
     
-    district = [ i for i in DG.nodes if xval[i,0] > 0.5 ]
-    assert nx.is_strongly_connected( DG.subgraph(district) )
-    complement = [ i for i in DG.nodes if xval[i,1] > 0.5 ]
-    if len(complement)==1 or nx.is_strongly_connected( DG.subgraph(complement) ):
-        m._districts.append( district )
+    district = [i for i in DG.nodes if xval[i,0] > 0.5]
+    assert nx.is_strongly_connected(DG.subgraph(district))
+    complement = [i for i in DG.nodes if xval[i,1] > 0.5]
+    if len(complement)==1 or nx.is_strongly_connected(DG.subgraph(complement)):
+        m._districts.append(district)
         m._number_of_districts += 1
         print("found district #", m._number_of_districts, ":", district)
     else:
         assert k==3
-        components = list( nx.strongly_connected_components( DG.subgraph(complement) ) )
-        populations = [ sum( DG.nodes[i]['TOTPOP'] for i in component ) for component in components ]
+        components = list(nx.strongly_connected_components(DG.subgraph(complement)))
+        populations = [sum(DG.nodes[i]['TOTPOP'] for i in component) for component in components]
         pop0_ok = L <= populations[0] and populations[0] <= U
         pop1_ok = L <= populations[1] and populations[1] <= U
         if len(components)==2 and pop0_ok and pop1_ok:
-            m._districts.append( district )
+            m._districts.append(district)
             m._number_of_districts += 1
             print("found district #", m._number_of_districts, ":", district)
         
     # force at least one vertex from complement must move to first_district. 
     # (this is generally not valid, but with 1-person deviation it will only cut off 
     #   this particular solution as all counties have nontrivial population)
-    m.cbLazy( gp.quicksum( m._x[i,0] for i in complement ) >= 1 )
+    m.cbLazy(gp.quicksum( m._x[i,0] for i in complement) >= 1)
     if m._number_of_districts == m._enumeration_limit:
         print("Enumeration limit has been reached. Exiting search.")
-        m.cbLazy( m._x[root,1] >= 1 )
-
-
+        m.cbLazy(m._x[root,1] >= 1)
 
 def to_string(my_list):
     string = str()
@@ -59,22 +57,22 @@ def to_string(my_list):
 
 def remove_duplicates(list_of_lists):
     input_len = len(list_of_lists)
-    set_of_strings = { to_string(my_list) for my_list in list_of_lists }
-    list_of_strings = [ string.split(',') for string in set_of_strings ]
-    new_lol = [ [ int(i) for i in my_list ] for my_list in list_of_strings ]
+    set_of_strings = {to_string(my_list) for my_list in list_of_lists}
+    list_of_strings = [string.split(',') for string in set_of_strings]
+    new_lol = [[int(i) for i in my_list] for my_list in list_of_strings]
     output_len = len(new_lol)
-    print("Removed this many duplicates:", input_len - output_len )
+    print("Removed this many duplicates:", input_len - output_len)
     return new_lol
 
 def enumerate_districts(G, L, U, k, root=None):
     m = gp.Model()
     x = m.addVars(G.nodes, k, vtype=GRB.BINARY)
 
-    m.addConstrs( x[i,0] + x[i,1] == 1 for i in G.nodes )
-    m.addConstr( gp.quicksum( G.nodes[i]['TOTPOP'] * x[i,0] for i in G.nodes ) >= L )
-    m.addConstr( gp.quicksum( G.nodes[i]['TOTPOP'] * x[i,0] for i in G.nodes ) <= U )
-    m.addConstr( gp.quicksum( G.nodes[i]['TOTPOP'] * x[i,1] for i in G.nodes ) >= (k-1)*L )
-    m.addConstr( gp.quicksum( G.nodes[i]['TOTPOP'] * x[i,1] for i in G.nodes ) <= (k-1)*U )
+    m.addConstrs(x[i,0] + x[i,1] == 1 for i in G.nodes)
+    m.addConstr(sum(G.nodes[i]['TOTPOP'] * x[i,0] for i in G.nodes) >= L)
+    m.addConstr(sum(G.nodes[i]['TOTPOP'] * x[i,0] for i in G.nodes) <= U)
+    m.addConstr(sum(G.nodes[i]['TOTPOP'] * x[i,1] for i in G.nodes) >= (k-1)*L)
+    m.addConstr(sum(G.nodes[i]['TOTPOP'] * x[i,1] for i in G.nodes) <= (k-1)*U)
 
     # symmetry breaking: fix root to be in first district
     x[root,0].LB = 1
@@ -84,12 +82,12 @@ def enumerate_districts(G, L, U, k, root=None):
 
     # add flow-based contiguity constraints (Shirabe) for first district
     f = m.addVars(DG.edges)
-    m.addConstrs( gp.quicksum( f[j,i] - f[i,j] for j in G.neighbors(i) ) == x[i,0] for i in G.nodes if i != root )
-    m.addConstrs( gp.quicksum( f[j,i] for j in G.neighbors(i) ) <= M * x[i,0] for i in G.nodes if i != root )
-    m.addConstr( gp.quicksum( f[j,root] for j in G.neighbors(root) ) == 0 )
+    m.addConstrs(sum(f[j,i] - f[i,j] for j in G.neighbors(i)) == x[i,0] for i in G.nodes if i != root)
+    m.addConstrs(sum(f[j,i] for j in G.neighbors(i)) <= M * x[i,0] for i in G.nodes if i != root)
+    m.addConstr(sum(f[j,root] for j in G.neighbors(root)) == 0)
 
     dist = nx.shortest_path_length(G, source=root)
-    m.setObjective( gp.quicksum( dist[i] * dist[i] * math.ceil(G.nodes[i]['TOTPOP']/1000) * x[i,0] for i in G.nodes ), GRB.MINIMIZE )
+    m.setObjective(sum(dist[i] * dist[i] * math.ceil(G.nodes[i]['TOTPOP']/1000) * x[i,0] for i in G.nodes), GRB.MINIMIZE)
     
     # add cut-based contiguity constraints for second district (whose root we do not know a priori)
     m.Params.LazyConstraints = 1
@@ -103,19 +101,14 @@ def enumerate_districts(G, L, U, k, root=None):
     m._enumeration_limit = 5001 
     m._districts = list()      
     m._number_of_districts = 0  
-
-    # MIP tolerances
     m.Params.IntFeasTol = 1e-7
     m.Params.FeasibilityTol = 1e-7
 
-    # solve
-    m.optimize( m._callback )
+    m.optimize(m._callback)
     
     # remove any possible duplicates
     m._districts = remove_duplicates(m._districts)
-    
-    districts = []
-    # report solutions
+    districts = list()
     print("We found the following",m._number_of_districts,"districts:\n")
     for p in range(m._number_of_districts):
         print("district #",p+1,"is", m._districts[p],"\n")
@@ -137,7 +130,7 @@ def enumerate_and_solve_k2_subproblems(G, deviation_persons, L, U, k, root=None,
         raise ValueError(f"Root with NAME20 '{root}' not found in graph.")
     root_node = matches[0]
 
-    full_plans = []
+    full_plans = list()
     first_districts = enumerate_districts(G, L, U, k, root= root_node)
 
     for idx, district_nodes in enumerate(first_districts):
@@ -159,6 +152,7 @@ def enumerate_and_solve_k2_subproblems(G, deviation_persons, L, U, k, root=None,
             full_plans.append(plan)
             print('plan =', plan)
             check_plan(G, plan,year=2020)
+            
     print("\nWe found the following plans:\n")
     for i, plan in enumerate(full_plans):
         print(f"Plan {i+1}: {plan}")
